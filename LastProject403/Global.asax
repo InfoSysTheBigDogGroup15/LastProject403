1c64ad3b30c1ef2b042b2eabdeb08d11fc7048ef
Jd <jddavenport46@gmail.com>
2018-12-12 18:36:19 -0700

    Add project files.

---
+++ LastProject403/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LastProject403.MvcApplication" Language="C#" %>
---
+++ LastProject403/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LastProject403.Startup))]
+namespace LastProject403
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
