cf2001693a25c7603fd3b84b13a344db5b8c45db
Jd <jddavenport46@gmail.com>
2018-12-12 19:31:00 -0700

    Jd is king

---
+++ LastProject403/Controllers/RealHomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LastProject403.Controllers
+{
+    public class RealHomeController : Controller
+    {
+        // GET: RealHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //get
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
