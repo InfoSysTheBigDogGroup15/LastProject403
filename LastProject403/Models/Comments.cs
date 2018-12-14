3998b03a9f57b90d333af88615baf2772f618146
Hoyong Jung <jhoyong57@gmail.com>
2018-12-13 21:14:03 -0700

    Hoyong created comments table model

---
+++ LastProject403/Models/Comments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LastProject403.Models
+{
+    [Table("Comments")]
+    public class Comments
+    {
+        [Key]
+        public int commentID { get; set; }
+
+        public String userName { get; set; }
+
+        public String comments { get; set; }
+
+        public String answers { get; set; }
+    }
+}
